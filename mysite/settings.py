e081a68c5daa6e3ba9038e091e90cf788c7def96
Yuji-207 <pooh.voxx@gmail.com>
2020-08-20 01:05:23 +0900

    share_tripアプリケーションを作成

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'share_trip.apps.ShareTripConfig',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
