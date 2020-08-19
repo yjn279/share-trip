b9e37b15272a3939520383c0af702c1f2eb4d6a3
Yuji-207 <pooh.voxx@gmail.com>
2020-08-20 01:53:21 +0900

    itineraryモデルを作成

--- share_trip/models.py
+++ share_trip/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Itinerary(models.Model):
+
+    title = models.CharField(max_length=32)
+    comment = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    edited_date = models.DateTimeField(blank=True, null=True)
+
+    def plan(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
