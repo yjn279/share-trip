decfccc172260f8167809613a8889d2df3d41ac1
Yuji-207 <pooh.voxx@gmail.com>
2020-08-20 09:38:33 +0900

    adminページを設定

--- share_trip/models.py
+++ share_trip/models.py
@@ -8,8 +8,8 @@ class Itinerary(models.Model):
     title = models.CharField(max_length=32)
     comment = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(default=timezone.now)
-    edited_date = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)  # 編集できないようにする
+    edited_date = models.DateTimeField(blank=True, null=True)  # 編集できないようにする
 
     def plan(self):
         self.save()
